c070da05ad7a4d1959f271f915b897f3d337b16d
hgti32 <127024694+hgti32@users.noreply.github.com>
2026-06-26 12:24:14 +0300

    fix

--- frontend/src/pages/registration/RegisterConfirmPage.tsx
+++ frontend/src/pages/registration/RegisterConfirmPage.tsx
@@ -110,11 +110,7 @@ export default function EmailConfirmationPage() {
             </button>
           </span>
           <Link
-<<<<<<< HEAD
-            to="/catalog"
-=======
             to="/login"
->>>>>>> 6e044830eb83edd9ae9760ba104de4751896a408
             className="text-muted-foreground hover:text-foreground transition-colors self-start flex items-center gap-2 mt-4"
           >
             {t.backToLogin}
